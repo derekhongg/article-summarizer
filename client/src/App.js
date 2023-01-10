59ad8b5154cebc01622997bc3eb36d01fe596a89
derekhongg <hongderek@gmail.com>
2023-01-09 20:40:17 -0800

    adding some design

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav/Nav';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Nav/>
+        <Routes>
+          {/* <Route path="/" element={<Home/>}/> */}
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
